839fa0904530f38b70e8533d452dfa3df40662a6
Joydeep Nandi <80601095+joydeepnandi@users.noreply.github.com>
2021-08-12 22:10:54 +0530

    updated

--- BinarySearch.py
+++ BinarySearch.py
@@ -60,5 +60,55 @@ def countSubArray(arr,B):
         res += end-1
     return res
     
+# Max Continuous Series of 1s
+'''
+Find the position of zeros which when flipped will produce maximum continuous series of 1s.
+
+For this problem, return the indices of maximum continuous series of 1s in order.
+
+Example:
+
+Input : 
+Array = {1 1 0 1 1 0 0 1 1 1 } 
+M = 1
+
+Output : 
+[0, 1, 2, 3, 4] 
+'''
+
+def solve(arr,M):
+    start = 0
+    maxIdx = [0,0]
+    zerocount = 0
+    for end in range(len(arr)):
+        if arr[end]==0:
+            zerocount+=1
+        while zerocount>M:
+            if arr[start]==0:
+                zerocount-=1
+            start+=1
+        if end - start + 1 > maxIdx[1] - maxIdx[0] +1:
+            arr[0] = start
+            arr[1] = end
+    res = [i for i in range(maxIdx[0],maxIdx[1]+1)]
+    return res
+        
+#Array 3 Pointers
+'''
+Find i, j, k such that :
+max(abs(A[i] - B[j]), abs(B[j] - C[k]), abs(C[k] - A[i])) is minimized.
+'''
+def solve(A,B,C):
+    i,j,k = 0,0,0
+    while i<len(A) and j<len(B) and k<len(C):
+        ans = min(ans, max(A[i],B[j],C[k]) - min(A[i],B[j],C[k]))
+        minElem =  min(A[i],B[j],C[k])
+        if minElem == A[i]:
+            i+=1
+        elif minElem == B[j]:
+            j+=1
+        else:
+            k+=1
+    return ans
 
     
